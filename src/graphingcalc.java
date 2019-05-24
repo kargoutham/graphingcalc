cc1814e911829eb6bf20ef6dc82c3e2b4285835d
Goutham Karthi <Goutham Karthi@DESKTOP-464NCFE.attlocal.net>
2019-05-23 20:32:17 -0700

    almost done

--- src/graphingcalc.java
+++ src/graphingcalc.java
@@ -5,6 +5,7 @@ import javax.swing.JFrame;
 public class graphingcalc extends JFrame{
 	public static ArrayList<Point> coordinates = new ArrayList<Point>();
 	
+	
 	public static void main(String[] args) {
 		
 		int max = 0;
@@ -17,12 +18,12 @@ public class graphingcalc extends JFrame{
 		String end = input.nextLine();
 		int startVal = Integer.parseInt(start);
 		int endVal = Integer.parseInt(end);
-		for(int i = startVal ; i<=1000;i+=1000/endVal) {
+		for(int i = startVal ; i<=endVal;i++) {
 			coordinates.add(calculate(equation,i));
 			max = findMax(coordinates);
-//			System.out.println(calculate(equation,i));
+			System.out.println(calculate(equation,i));
 		}		
-		System.out.println(max);
+//		System.out.println(max);
 		graphingcalc c = new graphingcalc(max);
 	}
 	
@@ -41,7 +42,8 @@ public class graphingcalc extends JFrame{
 				}catch(Exception e) {
 					slope = 1;
 				}
-				yint = Double.parseDouble(equation.substring(equation.indexOf("+")+1, equation.length()));
+				yint = Double.parseDouble(equation.substring(equation.indexOf("+")+2, equation.length()));
+				System.out.println("slope: "+slope+"yint: "+yint);
 				
 			}
 			if(equation.contains("-")) {
@@ -50,8 +52,9 @@ public class graphingcalc extends JFrame{
 				}catch(Exception e) {
 					slope = 1;
 				}
-				yint = Double.parseDouble(equation.substring(equation.indexOf("-")+1, equation.length()));
+				yint = Double.parseDouble(equation.substring(equation.indexOf("-")+2, equation.length()));
 				yint*=-1;
+				System.out.println("slope: "+slope+"yint: "+yint);
 				
 			}else {
 				try {
@@ -60,6 +63,7 @@ public class graphingcalc extends JFrame{
 						slope = 1;
 					}
 				yint = 0;
+				System.out.println("slope: "+slope+"yint: "+yint);
 			}
 			y = slope*x + yint;
 			p.setLocation((int)x,(int)y);
@@ -71,8 +75,9 @@ public class graphingcalc extends JFrame{
 					}catch(Exception e) {
 						slope = 1;
 					}
-				yint = Double.parseDouble(equation.substring(equation.indexOf("+")+1, equation.length()));
+				yint = Integer.parseInt(equation.substring(equation.indexOf("+")+2, equation.length()));
 				power = Double.parseDouble(equation.substring(equation.indexOf("^")+1, equation.indexOf("+")-1));
+				System.out.println("slope: "+slope+"yint: "+yint+"power: "+power);
 			}
 			if(equation.contains("-")) {
 				try {
@@ -80,10 +85,11 @@ public class graphingcalc extends JFrame{
 				}catch(Exception e) {
 					slope = 1;
 				}
-				yint = Double.parseDouble(equation.substring(equation.indexOf("-")+1, equation.length()));
+				yint = Integer.parseInt(equation.substring(equation.indexOf("-")+2, equation.length()));
 				yint*=-1;
 				power = Double.parseDouble(equation.substring(equation.indexOf("^")+1, equation.indexOf("-")-1));
-				
+				System.out.println("slope: "+slope+"yint: "+yint+"power: "+power);
+			
 			}else {
 				try {
 					slope = Double.parseDouble(equation.substring(0, equation.indexOf("x")));
@@ -92,6 +98,7 @@ public class graphingcalc extends JFrame{
 					}
 				yint = 0;
 				power = Double.parseDouble(equation.substring(equation.indexOf("^")+1, equation.length()));
+				System.out.println("slope: "+slope+"yint: "+yint+"power: "+power);
 			}
 			y = slope*(Math.pow(x, power)) + yint;
 			p.setLocation((int)x,(int)y);
@@ -110,16 +117,21 @@ public class graphingcalc extends JFrame{
 	}
 	public graphingcalc(int y) {
 		setTitle("Graph"); 
-		setSize(1000,y);
+		setSize(1000,1000);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 
 
 	public void paint(Graphics g) {
-		for(int i = 0; i<coordinates.size();i++) {
-			g.drawLine((int)coordinates.get(i).getX(), (int)coordinates.get(i).getY(), (int)coordinates.get(i+1).getX(), (int)coordinates.get(i+1).getY());
+		double scaleY = 1000/(coordinates.get(coordinates.size()-1).getY()-coordinates.get(0).getY());
+		double scaleX = 1000/(coordinates.get(coordinates.size()-1).getX()-coordinates.get(0).getX());
+		int minX = (int)coordinates.get(0).getX();
+		
+		for(int i = 0; i<coordinates.size()-1;i++) {
+			g.drawLine((int)(coordinates.get(i).getX()*scaleX)-minX, 1000-(int)((coordinates.get(i).getY())*scaleY), (int)(coordinates.get(i+1).getX()*scaleX)-minX, 1000-(int)((coordinates.get(i+1).getY())*scaleY));
 		}
+		
 	}
 	
 
